e174fedd6cdea657a783272fa6408794efe3e482
edu <13965412+edwanyoike@users.noreply.github.com>
2020-08-19 00:16:32 +0300

    concerns

--- operation.py
+++ operation.py
@@ -1,43 +1,51 @@
 from StackMachine import StackOperations
 from inputs import Inputs
 
-#GATHERING INPUTS
+# GATHERING INPUTS
 myStack = StackOperations()
 str = str.upper(input("enter your string here, separate your words using whitespace: "))
 split = str.split()
 print(str)
 
-#OPERATIONAL FUNCTIONS
-#TODO why do you have these functions redefined in this module while they already exist in StackMachine
+
+# OPERATIONAL FUNCTIONS
+# TODO why do you have these functions redefined in this module while they already exist in StackMachine
 def pop():
     myStack.pop()
+
+
 def sub():
     myStack.push(myStack.sub())
+
+
 def dup():
     myStack.push(myStack.dup())
+
+
 def add():
     myStack.push(myStack.add())
 
-#CHECKLOOP
+
+# CHECKLOOP
 for item in split:
     try:
         int_value = int(item)
     except ValueError:
-        if item=="DUP":  #TODO are you considering case sensitivity in this comparision
+        if item == "DUP":  # TODO are you considering case sensitivity in this comparision
             dup()
-            print( myStack.get_stack())
-        elif item =="POP":
+            print(myStack.get_stack())
+        elif item == "POP":
             pop()
-            print( myStack.get_stack())
-        elif item=="+":
+            print(myStack.get_stack())
+        elif item == "+":
             add()
-            print( myStack.get_stack())
-        elif item=="-":
+            print(myStack.get_stack())
+        elif item == "-":
             sub()
-            print( myStack.get_stack())
-        else :
-            print("Your string of operations contain unknown please double check :" +item)
+            print(myStack.get_stack())
+        else:
+            print("Your string of operations contain unknown please double check :" + item)
     else:
         myStack.push(item)
-        print( myStack.get_stack())
-print( myStack.get_stack())
+        print(myStack.get_stack())
+print(myStack.get_stack())
